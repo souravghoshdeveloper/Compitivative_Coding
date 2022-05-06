78fece9f4d3db17e996d22049bf6f3850ebd8d77
Sourav Ghosh <infosourav04@gmail.com>
2022-05-06 17:11:03 +0530

    Initial commit

--- Day 2/5.py
+++ Day 2/5.py
@@ -2,10 +2,40 @@
 those four lines
 x=[]
 for i in range (6):
-x.append ( i**2 )
+x.append(i**2)
 print(x)
 
 by using comprehension logic we convert the four line to two lines of ABOVE prgm
 """
 t=[i**2 for i in range(6)]
 print(t)
+
+
+
+"""
+x=[]
+for i in [1,2,3]:
+    for j in [3,4,5]:
+        if i != j:
+            x.append ((i,j))
+print(x)"""
+
+# Soluction
+y=[(i,j )for i in [1,2,3] for j in [3,4,5] if i!=j ]
+print(y)
+
+
+# write a prgm to find out 1 to 10 from list comprehension
+"""
+list1 = []
+for i in range(0,10):
+    if (i %2 != 0):
+        list1.append(i)
+print(list1)"""
+
+# Soluction
+B=[i for i in range(0,10) if (i %2 != 0)]
+print(B)
+
+
+# write a prgm to find out factorial of file using list comprehension
