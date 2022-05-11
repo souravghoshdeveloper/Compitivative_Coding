210ac7b419e61136971c873eee22bd5e32921c20
Sourav Ghosh <infosourav04@gmail.com>
2022-05-11 14:58:48 +0530

    Initial commit

---
+++ Day 3/1.py
@@ -0,0 +1,4 @@
+def hello_world():
+    print("hello World")
+
+hello_world()
---
+++ Day 3/2.py
@@ -0,0 +1,8 @@
+principal = int(input("Enter Principal? "))
+rate = int(input("Enter rate of interest? "))
+time = int(input("Enter Time? "))
+
+def simple_interest(principal,rate,time):
+    print((principal*time*rate)/100)
+
+simple_interest(principal, rate,time)
