241d65e603b18e382cdef9bd0231414e41c4b9be
Sourav Ghosh <infosourav04@gmail.com>
2022-05-11 15:30:24 +0530

    Initial commit

--- Day 3/2.py
+++ Day 3/2.py
@@ -6,3 +6,15 @@ def simple_interest(principal,rate,time):
     print("Simple Interest",(principal*time*rate)/100)
 
 simple_interest(principal, rate,time)
+
+#-------------------------------------------------------
+def simple_interest(p,r,t):  return (p*t*r)/100
+print("simple interest: ", simple_interest(t=10,r=10,p=1900))
+
+#-------------------------------------------------------
+def func(name1,message,name2):
+    print("printing the message with",name1,",",message,", and",name2)
+func(name1="john",message="hello",name2="david")
+
+
+#-------------------------------------------------------
