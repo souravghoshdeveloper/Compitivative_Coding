b7157708b9e73003e0e9e61e25ed4857bcc5f20b
Sourav Ghosh <infosourav04@gmail.com>
2022-05-11 16:05:05 +0530

    Initial commit

--- Day 3/3.py
+++ Day 3/3.py
@@ -1,7 +1,7 @@
 # Lambda Function
 #if any variable containing lambda it is called function
-"""x = lambda a:a+10 # a is an argument and a+10 is an expression which got evaluated and returned.
-print("sum = ",x(20))"""
+x = lambda a:a+10 # a is an argument and a+10 is an expression which got evaluated and returned.
+print("sum = ",x(20))
 
 #---------------------------------
 # Lambda Function
