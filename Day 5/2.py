26c387405d147f9b1097050c76d6b21814559fc4
Sourav Ghosh <infosourav04@gmail.com>
2022-05-18 14:58:02 +0530

    Initial commit

---
+++ Day 5/2.py
@@ -0,0 +1,3 @@
+# pythone code to illuste cube of a number
+# showing difference bet def() and lambda()
+lambda_y= lambda y: y*y*y
---
+++ Day 5/3.py
@@ -0,0 +1,5 @@
+tables = [lambda x=x: x*10 for x in range(1,11)]
+for table in tables:
+    print(table())
+
+# solun
