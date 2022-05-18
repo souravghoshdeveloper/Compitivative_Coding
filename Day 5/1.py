b7ee14b3791fd58dfd45594d1a454ae825ce7d58
Sourav Ghosh <infosourav04@gmail.com>
2022-05-18 14:38:25 +0530

    Initial commit

---
+++ Day 5/1.py
@@ -0,0 +1,13 @@
+# f= lambda a:a*a
+# lambda a, b: a+b
+# (lambda x: x+1)(2)
+# programe to filter out only the even items from a list ,((((( lamba alawys return object , reduce alaways return list))))
+
+my_list = [1,5,4,6,8,11,3,12]
+new_list=list(filter(lambda x: (x%2==0),my_list))
+print(new_list)
+# map function
+my_list = [1,5,4,6,8,11,3,12]
+new_list=list(map(lambda x:(x%2==0),my_list)) # map is a iterable elements
+print(new_list)
+
